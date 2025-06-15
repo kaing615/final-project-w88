493bd9ae18659655df19f520a61cac948c19b5c9
kaing615 <kainguyen615@gmail.com>
2025-06-15 17:11:31 +0700

    fix baseURL

--- Client/src/api/client/private.client.js
+++ Client/src/api/client/private.client.js
@@ -1,37 +1,37 @@
-import axios from 'axios';
-import queryString from 'query-string';
+import axios from "axios";
+import queryString from "query-string";
 
-const baseURL = "http://127.0.0.1:5050/api/v1";
+const baseURL = "https://streamix-syie.onrender.com/api/v1";
 
 const privateClient = axios.create({
-    baseURL,
-    paramsSerializer: {
-        encode: params => queryString.stringify(params)
-    }
+  baseURL,
+  paramsSerializer: {
+    encode: (params) => queryString.stringify(params),
+  },
 });
 
 privateClient.interceptors.request.use(
-    async (config) => {
-        config.headers = {
-            ...config.headers,
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('actkn')}`
-        };
-        return config;
-    },
-    (error) => Promise.reject(error)
+  async (config) => {
+    config.headers = {
+      ...config.headers,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("actkn")}`,
+    };
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 privateClient.interceptors.response.use(
-    (response) => {
-        if (response && response.data) {
-            return response.data;
-        }
-        return response;
-    },
-    (error) => {
-        throw error.response.data;
+  (response) => {
+    if (response && response.data) {
+      return response.data;
     }
+    return response;
+  },
+  (error) => {
+    throw error.response.data;
+  }
 );
 
 export default privateClient;
--- Client/src/api/client/public.client.js
+++ Client/src/api/client/public.client.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
-import queryString from 'query-string';
+import axios from "axios";
+import queryString from "query-string";
 
-const baseURL = "http://127.0.0.1:5050/api/v1";
+const baseURL = "https://streamix-syie.onrender.com/api/v1";
 
 const publicClient = axios.create({
-    baseURL,
-    paramsSerializer: {
-        encode: params => queryString.stringify(params)
-    }
+  baseURL,
+  paramsSerializer: {
+    encode: (params) => queryString.stringify(params),
+  },
 });
 
 publicClient.interceptors.request.use(
-    async (config) => {
-        config.headers = {
-            ...config.headers,
-            'Content-Type': 'application/json'
-        };
-        return config;
-    },
-    (error) => Promise.reject(error)
+  async (config) => {
+    config.headers = {
+      ...config.headers,
+      "Content-Type": "application/json",
+    };
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 publicClient.interceptors.response.use(
-    (response) => {
-        if (response && response.data) {
-            return response.data;
-        }
-        return response;
-    },
-    (error) => {
-        throw error.response.data;
+  (response) => {
+    if (response && response.data) {
+      return response.data;
     }
+    return response;
+  },
+  (error) => {
+    throw error.response.data;
+  }
 );
 
 export default publicClient;
